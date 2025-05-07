f7694c9998b0a4d2779498a3bcb85c883e5e5b8e
Nir Drang <nirdrang@gmail.com>
2025-05-07 09:31:31 +0000

    can you please commit the restored version to github ?

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  devIndicators: {
+    allowedDevOrigins: [], // Keep the structure but empty the array
+  },
+};
+
+module.exports = nextConfig;
